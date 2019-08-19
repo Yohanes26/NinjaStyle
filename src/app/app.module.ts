e58be3a02a1f788ab35982313dda50139d36ec45
James <jkelvin18@hotmail.com>
2019-08-19 00:27:45 -0300

    criado componentes alerta, badge e breadcrumb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AlertsComponent } from './contents/alerts/alerts.component';
+import { BadgeComponent } from './contents/badge/badge.component';
+import { BreadcrumbComponent } from './contents/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AlertsComponent,
+    BadgeComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
